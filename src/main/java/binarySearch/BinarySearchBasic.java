ab193534a32cfbb02718686a815b5c454aeafcd1
mridusmita roy <mridusmita.roy@apiseroinc.onmicrosoft.com>
2021-08-05 02:26:15 +0530

    binarySearch

---
+++ src/main/java/binarySearch/BinarySearchBasic.java
@@ -0,0 +1,24 @@
+package binarySearch;
+
+public class BinarySearchBasic {
+	public static void main(String args[]) {
+		int arr[] = {5};
+		System.out.println("ans is:" + binarySearch(arr, 5));
+	}
+
+	public static boolean binarySearch(int arr[], int key) {
+		int low = 0;
+		int high = arr.length - 1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (arr[mid] == key) {
+				return true;
+			} else if (arr[mid] < key) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return false;
+	}
+}
